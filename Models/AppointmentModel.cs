cb825077c59480945d69ccd2ec37db63742804a2
Kieran Emery <kizwiz@hotmail.co.uk>
2022-11-04 18:35:21 +0000

    Adds some address fields to the CustomerModel: Street, City, ZIP code, email and phone.

--- Models/AppointmentModel.cs
+++ Models/AppointmentModel.cs
@@ -10,6 +10,28 @@ namespace AppointmentMaker.Models
         [DisplayName("Patient's full name")]
         public string PatientName { get; set; }
 
+        [StringLength(40, MinimumLength = 4)]
+        [DisplayName("Patient's street")]
+        public string? PatientStreet { get; set; }
+
+        [StringLength(30, MinimumLength = 2)]
+        [DisplayName("Patient's city")]
+        public string? PatientCity { get; set; }
+
+        [StringLength(10, MinimumLength = 2)]
+        [DisplayName("Patient's ZIP code")]
+        public string? PatientZIP { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 4)]
+        [DisplayName("Patient's email")]
+        public string PatientEmail { get; set; }
+
+        [Required]
+        [StringLength(12, MinimumLength = 5)]
+        [DisplayName("Patient's phone number")]
+        public int PatientPhone { get; set; }
+
         [DataType(DataType.Date)]
         [DisplayName("Appointment Request Date")]
         public DateTime AppointmentDate { get; set; }
