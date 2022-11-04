71333b5c73da78eea10719595386111365c84369
Kieran Emery <kizwiz@hotmail.co.uk>
2022-11-04 13:18:02 +0000

    Using ComponentModel class to allow validation to allow a display name to display on discreen when the form shows.

--- Models/AppointmentModel.cs
+++ Models/AppointmentModel.cs
@@ -1,11 +1,22 @@
-﻿namespace AppointmentMaker.Models
+﻿using System.ComponentModel;
+
+namespace AppointmentMaker.Models
 {
     public class AppointmentModel
     {
+        [DisplayName("Patient's full name")]
         public string PatientName { get; set; }
+
+        [DisplayName("Appointment Request Date")]
         public DateTime AppointmentDate { get; set; }
+
+        [DisplayName("Patient's Approximate Net Worth")]
         public decimal PatientNetWorth { get; set; }
+
+        [DisplayName("Primary Doctor's Last Name")]
         public string DoctorName { get; set; }
+
+        [DisplayName("Patient's Perceived Level of Pain (1 low to 10 higH)")]
         public int PainLevel { get; set; }
     }
 }
