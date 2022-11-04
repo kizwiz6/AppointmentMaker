2f6745c3f4428d953fde37f16285e26c13d7e244
Kieran Emery <kizwiz@hotmail.co.uk>
2022-11-04 13:13:54 +0000

    Add project files.

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentMaker.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        /// <summary>
+        /// Show a list (which will be Index).
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Create a view for the create option. Creates a new appointment.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/AppointmentModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AppointmentMaker.Models
+{
+    public class AppointmentModel
+    {
+        public string PatientName { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public decimal PatientNetWorth { get; set; }
+        public string DoctorName { get; set; }
+        public int PainLevel { get; set; }
+    }
+}
